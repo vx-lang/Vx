9aa02490236295d713a1a5451bf533a5d260fe20
Aditya <1894981+hiraditya@users.noreply.github.com>
2026-05-16 03:39:13 -0700

    test: Fix syntax error in module_api_test

--- tests/module_api_test.rs
+++ tests/module_api_test.rs
@@ -3,12 +3,12 @@ use vxc::parse_module;
 #[test]
 fn test_parse_module_api() {
     let source = "
-        fn hello_world() {
+        fn hello_world() -> Tensor {
             let x = 42;
         }
     ";
     let module = parse_module(source);
-    assert!(module.is_ok());
+    assert!(module.is_ok(), "Parse failed: {:?}", module.err());
     let module = module.unwrap();
     assert_eq!(module.functions.len(), 1);
     assert_eq!(module.functions[0].name, "hello_world");
