889792336fc9779d5fc2fca846ae759c26fc6cdf
Aditya <1894981+hiraditya@users.noreply.github.com>
2026-05-16 00:15:14 -0700

    fix: Update verify_subtyping_bounds test to use LocalWorkerState

--- tests/borrow_test.rs
+++ tests/borrow_test.rs
@@ -3,7 +3,9 @@ use vxc::borrow::verify_subtyping_bounds;
 
 #[test]
 fn test_fast_path_variance_checks() {
-    let session = std::sync::Arc::new(vxc::session::GlobalSession::new(1));
+    let global_session = std::sync::Arc::new(vxc::session::GlobalSession::new(1));
+    let worker = vxc::session::LocalWorkerState::new(global_session.clone());
+
     // Type A: Variance = 0, Region = 0 ('static)
     let mut type_a = TypeId::new(0, 0, 0, 0);
     type_a.try_set_fast_param(0, 0, 0).unwrap();
@@ -13,11 +15,11 @@ fn test_fast_path_variance_checks() {
     type_b.try_set_fast_param(0, 1, 0).unwrap();
 
     // 'static (0) can be coerced to 'a (1)
-    assert!(verify_subtyping_bounds(&type_a, &type_b, &session));
+    assert!(verify_subtyping_bounds(&type_a, &type_b, &worker));
 
     // 'a (1) cannot be coerced to 'static (0)
-    assert!(!verify_subtyping_bounds(&type_b, &type_a, &session));
+    assert!(!verify_subtyping_bounds(&type_b, &type_a, &worker));
 
     // Exact structural match
-    assert!(verify_subtyping_bounds(&type_a, &type_a, &session));
+    assert!(verify_subtyping_bounds(&type_a, &type_a, &worker));
 }
